db66bb739260b3c2a1c505f04bd3ce5dcd870c6c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-09 16:06:45 +0300

    Sorted the imports for the script

--- auth/authentication.py
+++ auth/authentication.py
@@ -1,6 +1,8 @@
 """The file with the authentication logic"""
+from datetime import datetime, timedelta
+from typing import Optional
+
 import jwt
 from fastapi import HTTPException, Security
 from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 from passlib.context import CryptContext
-from datetime import datetime, timedelta
