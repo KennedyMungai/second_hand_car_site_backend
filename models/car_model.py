29b03066390ae8c295f5a2da8ffa3d12806454dd
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-09 15:45:08 +0300

    Updated the car models

--- models/car_model.py
+++ models/car_model.py
@@ -67,11 +67,11 @@ class CarBase(MongoBaseModel):
         MongoBaseModel (MongoBaseModel): The base model for the mongo
     """
     brand: str = Field(..., min_length=3)
-    make: str = Field(..., min_length=3)
-    year: int = Field(...)
+    make: str = Field(..., min_length=1)
+    year: int = Field(..., gt=1975, lt=2023)
     price: int = Field(...)
     km: int = Field(...)
-    cm3: int = Field(...)
+    cm3: int = Field(..., gt=600, lt=8000)
 
 
 class CarUpdate(MongoBaseModel):
@@ -89,4 +89,4 @@ class CarDB(CarBase):
     Args:
         CarBase (CarBase): The car base model
     """
-    pass
+    owner: str = Field(...)
