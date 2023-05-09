69a92213e4194f42048c72362447a80f0badd511
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-09 16:06:17 +0300

    Added a bunch of imports to the authenticationscript

--- auth/authentication.py
+++ auth/authentication.py
@@ -1,1 +1,6 @@
 """The file with the authentication logic"""
+import jwt
+from fastapi import HTTPException, Security
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
+from passlib.context import CryptContext
+from datetime import datetime, timedelta
