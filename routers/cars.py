fef0f1dfde98e930b517b51dfbf6d3fd002a6a34
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-09 11:16:57 +0300

    Updated the name of the database being queried

--- routers/cars.py
+++ routers/cars.py
@@ -27,7 +27,8 @@ async def list_cars(
     if brand:
         query["brand"] = brand
 
-    full_query = request.app.mongodb["cars1"].find(query).sort("_id", 1)
+    full_query = request.app.mongodb["second_hand_cars"].find(
+        query).sort("_id", 1)
 
     results = [CarDB(**raw_car) async for raw_car in full_query]
 
@@ -46,8 +47,8 @@ async def create_car(request: Request, car: CarBase = Body(...)):
         JSONResponse: The response is in JSON format
     """
     car = jsonable_encoder(car)
-    new_car = await request.app.mongodb["cars1"].insert_one(car)
-    created_car = await request.app.mongodb["cars1"].find_one({"_id": new_car.inserted_id})
+    new_car = await request.app.mongodb["second_hand_cars"].insert_one(car)
+    created_car = await request.app.mongodb["second_hand_cars"].find_one({"_id": new_car.inserted_id})
 
     return JSONResponse(status_code=status.HTTP_201_CREATED, content=created_car)
 
@@ -63,7 +64,7 @@ async def show_car(car_id: str, request: Request):
     Raises:
         HTTPException: Raises a 404 not found if not found
     """
-    if (car := await request.app.mongodb["cars1"].find_one({"_id": car_id})) is not None:
+    if (car := await request.app.mongodb["second_hand_cars"].find_one({"_id": car_id})) is not None:
         return CarDB(**car)
 
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
