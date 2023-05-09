3f1387715bf325617b8d166b4de603e56ac46930
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-09 14:17:37 +0300

    Solved a simple error

--- models/user_model.py
+++ models/user_model.py
@@ -1,1 +1,8 @@
 """Created the User model file"""
+from enum import Enum
+from typing import Optional
+
+from bson import ObjectId
+from email_validator import EmailNotValidError, validate_email
+from fastapi import APIRouter
+from pydantic import BaseModel, EmailStr, Field, validator
--- routers/users.py
+++ routers/users.py
@@ -1,10 +1,4 @@
 """The Users router file"""
-from enum import Enum
-from typing import Optional
-
-from bson import ObjectId
-from email_validator import EmailNotValidError, validate_email
 from fastapi import APIRouter
-from pydantic import BaseModel, EmailStr, Field, validator
 
 users_router = APIRouter(prefix="Users", tags=["Users"])
