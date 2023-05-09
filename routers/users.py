1b06814c46b3d40acb1e3fc380f51311540e7e0c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-09 16:26:02 +0300

    Created an instance of the AuthHandler class:

--- routers/users.py
+++ routers/users.py
@@ -1,4 +1,9 @@
 """The Users router file"""
-from fastapi import APIRouter
+from fastapi import APIRouter, Request, Body, status, HTTPException, Depends
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
+from models.user_model import UserBase, LoginBase, CurrentUser
+from auth.authentication import AuthHandler
 
 users_router = APIRouter(prefix="Users", tags=["Users"])
+auth_handler = AuthHandler()
