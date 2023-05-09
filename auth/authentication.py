e2d509538a76f184d89519a6ff906b48bcf85981
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-09 16:10:41 +0300

    Created the Authorization class for the script

--- auth/authentication.py
+++ auth/authentication.py
@@ -6,3 +6,14 @@ import jwt
 from fastapi import HTTPException, Security
 from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
 from passlib.context import CryptContext
+import os
+from dotenv import find_dotenv, load_dotenv
+
+
+load_dotenv(find_dotenv())
+
+
+class Authorization():
+    security = HTTPBearer()
+    pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+    secret = os.environ.get("SECRET_KEY")
