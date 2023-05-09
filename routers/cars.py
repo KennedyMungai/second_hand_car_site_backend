8302ba86b535231d97ed850ca4d11eba1cac68e5
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-09 10:55:53 +0300

    Added docstrings to the create_car function

--- routers/cars.py
+++ routers/cars.py
@@ -19,6 +19,15 @@ async def list_cars():
 
 @cars.post("/", response_description="Create a new car")
 async def create_car(request: Request, car: CarBase = Body(...)):
+    """This is an endpoint that creates a car
+
+    Args:
+        request (Request): A request object
+        car (CarBase, optional): The template for the Car data. Defaults to Body(...).
+
+    Returns:
+        JSONResponse: The response is in JSON format
+    """
     car = jsonable_encoder(car)
     new_car = await request.app.mongodb["cars1"].insert_one(car)
     created_car = await request.app.mongodb["cars1"].find_one({"_id": new_car.inserted_id})
