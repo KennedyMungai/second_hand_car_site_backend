2cb32751b867d74720f2440358a2456af3959d8e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-09 10:23:02 +0300

    Defined a simple enspoint to list all the cars

--- app/routers/cars.py
+++ app/routers/cars.py
@@ -1,1 +1,14 @@
 """The cars router file"""
+from fastapi import APIRouter
+
+cars = APIRouter(prefix='cars', tags=['cars'])
+
+
+@cars.get("/", response_description="List all cars")
+async def list_cars():
+    """An endpoint to list all cars in the database
+
+    Returns:
+        dict[str, str]: A simple placeholder message
+    """
+    return {"data": "All cars will go here"}
