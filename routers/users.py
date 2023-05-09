178d541115020c36b3fee3aaf267bca2ae1d0639
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-09 14:16:27 +0300

    Added a bunch of imports to the script

--- routers/users.py
+++ routers/users.py
@@ -1,4 +1,10 @@
 """The Users router file"""
+from enum import Enum
+from typing import Optional
+
+from bson import ObjectId
+from email_validator import EmailNotValidError, validate_email
 from fastapi import APIRouter
+from pydantic import BaseModel, EmailStr, Field, validator
 
 users_router = APIRouter(prefix="Users", tags=["Users"])
