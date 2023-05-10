e2b5aefbabaf7fdb9a73436f8bad011dc54f3e50
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-10 12:52:02 +0300

    Wrote the rest of the register user code

--- routers/users.py
+++ routers/users.py
@@ -20,3 +20,16 @@ async def register_user(_request: Request, _new_user: UserBase = Body(...)) -> U
     """
     _new_user.password = auth_handler.get_password_hash(_new_user.password)
     _new_user = jsonable_encoder(_new_user)
+
+    if (existing_email := await _request.app.mongodb["user"].find_one({"email": _new_user["email"]}) is not None):
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT,
+                            detail=f"User with email {_new_user['email']} already exists")
+
+    if (existing_username := await _request.app.mongodb["users"].find_one({"username": _new_user["username"]}) is not None):
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT,
+                            detail=f"User with username {_new_user['username']} already exists")
+
+    user = await _request.app.mongodb["user"].insert_one(_new_user)
+    created_user = await _request.app.mongodb["user"].find_one({"_id": user.inserted_id})
+
+    return JSONResponse(status_code=status.HTTP_201_CREATED, content=created_user)
