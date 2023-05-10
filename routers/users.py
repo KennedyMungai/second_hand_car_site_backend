de92730599d5de1f069b47d6e02dc4d534dbb43b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-05-10 11:39:00 +0300

    Created an endpoint to register users

--- routers/users.py
+++ routers/users.py
@@ -8,3 +8,15 @@ from models.user_model import CurrentUser, LoginBase, UserBase
 
 users_router = APIRouter(prefix="Users", tags=["Users"])
 auth_handler = AuthHandler()
+
+
+@users_router.post("/register", response_description="User Registration")
+async def register_user(request: Request, _new_user: UserBase = Body(...)) -> UserBase:
+    """An endpoint to register users
+
+    Args:
+        request (Request): Register Object
+        _new_user (UserBase, optional): User Data. Defaults to Body(...).
+    """
+    _new_user.password = auth_handler.get_password_hash(_new_user.password)
+    _new_user = jsonable_encoder(_new_user)
